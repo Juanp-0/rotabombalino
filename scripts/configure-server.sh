95eff45b60b1fb17dc2e71f6eda8768356de255a
Zapoye18 <al02953375@tecmilenio.mx>
2025-05-10 16:53:26 -0600

    Primera version de la Pipeline

---
+++ scripts/configure-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+cd ~/minecraft-server/mods
+rm -rf ./*
+aws s3 cp s3://minecraft-server-src/src/ ./ --recursive
+cd ..
+nohup java -jar forge-1.21.1-52.1.1-shim.jar > server.log 2>&1 &
