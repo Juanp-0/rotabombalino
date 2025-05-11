fd69d3fef99d23f1bf6d54017d319e49fc3366a7
Zapoye18 <al02953375@tecmilenio.mx>
2025-05-10 20:16:48 -0600

    Update configure-server.sh
    limito los recursos gastados en iniciar el servidor

--- .github/workflows/scripts/configure-server.sh
+++ .github/workflows/scripts/configure-server.sh
@@ -5,4 +5,4 @@ cd ~/minecraft-server/mods
 rm -rf ./*
 aws s3 cp s3://minecraft-server-src/src/ ./ --recursive
 cd ..
-nohup java -jar forge-1.21.1-52.1.1-shim.jar > server.log 2>&1 &
+nohup java -Xmx2G -Xms1G -jar forge-1.21.1-52.1.1-shim.jar > server.log 2>&1 &
